4e0316d790cbe0ddc4b7a36a0249927775f45c2d
Cameron <camearle20@users.noreply.github.com>
2018-07-14 02:41:12 -0400

    Add some more files

---
+++ autolib/src/main/kotlin/org/team401/autolib/SomeAdapter.kt
@@ -0,0 +1,10 @@
+package org.team401.autolib
+
+/**
+ * @author Cameron Earle
+ * @version 7/14/2018
+ *
+ */
+object SomeAdapter {
+    //Just a placeholder to get the directory structure to push
+}
---
+++ robot/src/main/kotlin/org/team401/rewrite2018/Setup.kt
@@ -0,0 +1,14 @@
+package org.team401.rewrite2018
+
+import org.snakeskin.dsl.*
+
+/**
+ * @author Cameron Earle
+ * @version 7/14/2018
+ *
+ */
+
+@Setup
+fun setup() {
+
+}
